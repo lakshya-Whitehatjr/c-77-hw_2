e0875291c310bfed350cf9a7497f5f48e98f11b3
lakshya-Whitehatjr <83779709+lakshya-Whitehatjr@users.noreply.github.com>
2021-07-18 17:57:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var first_name = "Lakshya";
+console.log (" My First Name is ".concat(first_name));
+var last_name = "Agiwal";
+console.log (" My Last Name is ".concat(last_name));
+var full_name = "Lakshya Agiwal";
+console.log ("My full Name is ".concat(full_name));
+var a = 20;
+console.log("A = ".concat (a) );
+var b = 10;
+console.log("B = ".concat (b) );
+console.log ( "Sum of A and B is ".concat(a + b));
+console.log ( "Difference of A and B is ".concat(a - b));
+console.log ( "Product of A and B is ".concat(a * b));
+console.log ( "Quotient  of A and B is ".concat(a / b));
+console.log ( "Square of A  is ".concat(Math.pow(a,2)));
+console.log ( "Square of B  is ".concat(Math.pow(b,2)));
+console.log ( "Cube of A  is ".concat(Math.pow(a,3)));
+console.log ( "Cube of B  is ".concat(Math.pow(b,3)));
+console.log ( "Square Root of A  is ".concat(Math.sqrt(a)));
+console.log ( "Square Root of B  is ".concat(Math.sqrt(b)));
+console.log ( "Cube Root of A  is ".concat(Math.cbrt(a)));
+console.log ( "Cube Root of B  is ".concat(Math.cbrt(b)));
+
+
+
+
+
+
